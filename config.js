6094b3b93253a9971c2e37f7fd4e2296080827cf
Alex Clapperton <Alex.Clapperton@codecomputerlove.com>
2018-02-08 13:07:41 +0000

    :art: Update pa11y dashboard

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ if (fs.existsSync(jsonPath)) {
 		readonly: env('READONLY', 'false') === 'true',
 
 		webservice: env('WEBSERVICE_URL', {
-			database: env('WEBSERVICE_DATABASE', 'MONGO_DB'),
+			database: env('WEBSERVICE_DATABASE', ENV['MONGO_DB']),
 			host: env('WEBSERVICE_HOST', '0.0.0.0'),
 			port: Number(env('WEBSERVICE_PORT', '3000')),
 			cron: env('WEBSERVICE_CRON', false)
