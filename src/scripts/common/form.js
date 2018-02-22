7958b2f53a94f7d6715a4151d7948ce6efcb0cb4
Alex Clapperton <Alex.Clapperton@codecomputerlove.com>
2018-02-22 12:44:28 +0000

    :art: Update form styles and js

--- src/scripts/common/form.js
+++ src/scripts/common/form.js
@@ -1,15 +1,23 @@
 module.exports = (function() {
   const inputs = $('[data-input]');
 
-  function checkIfInputEmpty() {
-    const test = inputs.filter(input => input.value === '');
+  function checkFilledInputs() {
+    const filledInputs = inputs.filter((index, value) => $(value).val().length > 0);
+    const filledInputsLabel = $(filledInputs).parent().parent().find('[data-label]');
+    const emptyInputs = inputs.filter((index, value) => $(value).val().length === 0);
+    const emptyInputsLabel = $(emptyInputs).parent().parent().find('[data-label]');
 
-    console.log(test, 'test');
+    filledInputs.addClass('form__input--filled');
+    filledInputsLabel.addClass('form__label--hidden');
+    emptyInputs.removeClass('form__input--filled');
+    emptyInputsLabel.removeClass('form__label--hidden');
   }
 
   function init() {
     if (inputs.length > 0) {
-      checkIfInputEmpty();
+      inputs.change(checkFilledInputs);
+      inputs.keyup(checkFilledInputs);
+      checkFilledInputs();
     }
   }
 
